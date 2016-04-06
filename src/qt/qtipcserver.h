14e1cb12a395962afa38881d80b4b0e7b61849af
Panther <panthercoin@protonmail.com>
2016-04-06 12:18:59 +0200

    Pink

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define PantherCoin-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "PantherCoinURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
